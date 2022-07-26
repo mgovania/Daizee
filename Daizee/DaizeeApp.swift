83f5d0e01c867ac6808ab11e04d8b7c9444b5737
Mega Govania <megagovania@gmail.com>
2022-07-26 14:39:26 +0700

    Initial Commit

---
+++ Daizee/DaizeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaizeeApp.swift
+//  Daizee
+//
+//  Created by Mega Govania on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DaizeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
